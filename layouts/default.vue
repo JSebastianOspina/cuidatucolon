15c0f840eedcbb07c48fe5503840acdba7585cb5
Sebastian Ospina <sebastianospinacontacto@gmail.com>
2020-10-29 01:00:15 -0500

    height

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-screen flex justify-center">
+  <div class="contenedor h-screen flex justify-center bg-red-200">
     <div class="custom-shape-divider-top-1603949195">
     <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
         <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" class="shape-fill"></path>
@@ -19,10 +19,24 @@
 </div>
   </div>
 </template>
-
+<script>
+export default {
+  mounted(){
+    console.log("ndsds");
+    let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+}
+</script>
 <style>
 body {
+  
   background-color: #f4f5fa;
+}
+.contenedor{
+    height: 100vh; /* Fallback for browsers that do not support Custom Properties */
+  height: calc(var(--vh, 1vh) * 100);
 }
 .custom-shape-divider-bottom-1603949143 {
     position: absolute;
