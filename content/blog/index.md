d99e0097267a6eeb50cfcae4e5d1c90a20579312
Sebastian Ospina <sebastianospinacontacto@gmail.com>
2020-12-02 13:49:48 -0600

    test

--- content/blog/index.md
+++
@@ -1,3 +0,0 @@
-# Este es el titulo de la paginación
-
-## Este es un subtitulo
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,15 +1,20 @@
 <template>
-  <nuxt-link to="hola">
-      Clic aca
-  </nuxt-link>
+  <div>
+    <div v-for="item in articulos" :key="item.slug">
+      <nuxt-link :to="item.slug"> {{item.slug}}</nuxt-link>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  async asyncData({ $content }) {
+    const articulos = await $content("blog").fetch();
+    console.log(articulos);
+    return { articulos };
+  },
+};
 </script>
 
 <style>
-
 </style>
